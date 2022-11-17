ad95bf12bbd823cc8813847265f0af5e17a6c084
FilipusKevin <filipuskevin9@gmail.com>
2022-11-17 14:34:39 +0700

    GD11_C_0730

---
+++ app/src/main/java/com/example/gd8_c_0730/api/MahasiswaApi.kt
@@ -0,0 +1,13 @@
+package com.example.gd8_c_0730.api
+
+class MahasiswaApi {
+    companion object{
+        val BASE_URL = ""
+
+        val GET_ALL_URL = BASE_URL + "mahasiswa/"
+        val GET_BY_ID_URL = BASE_URL + "mahasiswa/"
+        val ADD_URL = BASE_URL + "mahasiswa"
+        val UPDATE_URL = BASE_URL + "mahasiswa/"
+        val DELETE_URL =  BASE_URL + "mahasiswa/"
+    }
+}
